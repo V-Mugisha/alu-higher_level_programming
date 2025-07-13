40926adf7cf4ce9b4b9934b5f6bcfb0ad23fc9ab
Victor Mugisha Shyaka <m.victor@alustudent.com>
2025-07-12 22:19:49 -0700

    update

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-''function that returns the dictionary description with simple data structure
-''
+'''function that returns the dictionary description with simple data structure
+'''
 
 
 def class_to_json(obj):
-    ''module class_to_json
+    '''module class_to_json
        returns builds a dictionary
-    ''
+    '''
     return obj.__dict__
