e2a777309daae924e462c587be3f4b661c415cc8
Victor Mugisha Shyaka <m.victor@alustudent.com>
2025-07-12 22:20:25 -0700

    update

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-'' a class Student that defines a student
-''
+''' a class Student that defines a student
+'''
 
 
 class Student:
-    ''module class student
-    ''
+    '''module class student
+    '''
 
     def __init__(self, first_name, last_name, age):
-        ''method __init__
-        ''
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        ''method to_json
-        ''
+        '''method to_json
+        '''
         return self.__dict__
