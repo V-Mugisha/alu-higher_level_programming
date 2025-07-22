d2f379bc64147d976fe0b59356fb948882e0bd8d
Victor Mugisha Shyaka <m.victor@alustudent.com>
2025-07-22 06:30:04 -0700

    PEP8

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""
-Python script that takes your GitHub credentials (username and personal access token)
-and uses the GitHub API to display your user ID.
+"""Python script that takes GitHub credentials (username and personal access token)
+and uses the GitHub API to display the user ID.
 Uses Basic Authentication with a personal access token (requires read:user permission).
 """
 
 import requests
 import sys
 
+
 if __name__ == '__main__':
     # Get username and personal access token from command line arguments
     username = sys.argv[1]
     token = sys.argv[2]
-    
+
     # GitHub API endpoint for user information
     url = "https://api.github.com/user"
-    
+
     try:
         # Make GET request with Basic Authentication
         response = requests.get(url, auth=(username, token))
-        
+
         # Check if request was successful (status code 200)
         if response.status_code == 200:
             user_data = response.json()
