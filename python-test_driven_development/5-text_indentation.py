6efa2b28c566168f8de9080e05f9bc6b2bd7b2de
Victor Mugisha Shyaka <m.victor@alustudent.com>
2025-07-25 09:14:44 -0700

    Text Indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,4 +12,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     c = 0
-    while c < len(text) and text[c] == 
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
